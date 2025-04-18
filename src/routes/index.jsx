5252cbd851d8034935425c9b7b0c1955504d9dda
Amir Alsayed <amyralsyd367@gmail.com>
2025-04-18 23:37:00 +0200

    adding login page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '../pages/dashboard/MainLayout';
 import NotFound from '../pages/errors/NotFound';
 import ProtectedRoute from '../components/common/ProtectedRoute';
-
+import LoginPage from '../pages/auth/Login';
 const AppRoutes = () => (
     <Routes>
         {/* Standalone routes for login and signup (no MainLayout/Sidebar) */}
-        <Route path="/login" element={<NotFound />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<NotFound />} />
 
         {/* Parent route with MainLayout and Sidebar */}
